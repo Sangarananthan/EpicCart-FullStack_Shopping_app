2e743783e3073358375cf66b3b29ec9304fa3ade
sangarananthan <sangarsmart16@gmail.com>
2024-12-31 19:31:47 +0530

    Lucid react package error

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173", // Vite's default port
   "http://localhost:3000",
+  "http://localhost:5000",
   "https://epic-cart-wheat.vercel.app/",
 ];
 
