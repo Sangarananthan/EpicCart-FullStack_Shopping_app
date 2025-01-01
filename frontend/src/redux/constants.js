71555e1df82bc08ca36af00c90376da4b45eab6b
sangarananthan <sangarsmart16@gmail.com>
2025-01-01 10:28:21 +0530

    commit

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://epic-cart.onrender.com";
+export const BASE_URL = "https://epic-cart.onrender.com/";
 export const USERs_URL = "/api/users/";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://epic-cart.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
-      'lucide-react': 'lucide-react/dist/esm/lucide-react'
-    }
-  }
+      "lucide-react": "lucide-react/dist/esm/lucide-react",
+    },
+  },
 });
